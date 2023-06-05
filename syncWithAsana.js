17ef626de10d2fd505b772fe2a78adf10858db0c
prernadgarsole <135641587+prernadgarsole@users.noreply.github.com>
2023-06-05 15:19:05 -0700

    Create syncWithAsana.js

---
+++ syncWithAsana.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const asanaApiKey = process.env.ASANA_API_KEY;
+const asanaProjectId = process.env.ASANA_PROJECT_ID;
+
+async function createTask(title, description) {
+  const response = await axios.post(
+    `https://app.asana.com/api/1.0/projects/${asanaProjectId}/tasks`,
+    {
+      data: {
+        name: title,
+        notes: description,
+      },
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${asanaApiKey}`,
+      },
+    }
+  );
+
+  return response.data.data;
+}
+
+// Example usage:
+createTask('Example task', 'This is an example task created from GitHub');
